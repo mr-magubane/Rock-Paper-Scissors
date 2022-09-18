f39db968a5c3f5439c45b541915919805dc21273
Mlondi <mlondimagubane@gmail.com>
2022-09-19 01:15:03 +0200

    Added message after game

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hi')
+function getComputerChoice() {
+    let guessValue = Math.floor(Math.random()*3+1);
+    if (guessValue === 1){
+        return "rock";
+    } else if (guessValue === 2){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+let playerSelection = toLowerCase(prompt("Selection:"));
+
+function game(ComputerChoice, playerChoice) {
+    let sameChoice = false;
+
+    while (! sameChoice) {
+        if (ComputerChoice === "rock") {
+            if (playerChoice === "paper") {
+                console.log("You win Rock beats paper");
+            }
+        }
+    }
+}
+console.log(game(getComputerChoice(), playerSelection))
