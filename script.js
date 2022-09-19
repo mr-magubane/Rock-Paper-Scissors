b41fff58b3024dabf564b19855bafa78663d7013
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-19 13:57:47 +0200

    completed assignment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice() {
     let guessValue = Math.floor(Math.random()*3+1);
     if (guessValue === 1){
@@ -8,32 +9,59 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-const compSelection = getComputerChoice()
-const playerSelection = prompt("Selection:").toLowerCase();
 
-function playRound(ComputerChoice, playerChoice) {
-    
-    if (ComputerChoice == "rock"){
-        if (playerChoice == "paper"){
+const NUMBER_OF_ROUNDS = 5;
+
+function playRound() {
+	
+	var compSelection = getComputerChoice()
+	var playerSelection = prompt("Selection:").toLowerCase();
+	console.log("You:",playerSelection);
+	console.log("Computer:",compSelection);
+
+    if (compSelection == "rock"){
+        if (playerSelection == "paper"){
             return "You win! Paper beats Rock";
-        } else if (playerChoice == "scissors"){
+        } else if (playerSelection == "scissors"){
             return "You lose! Rock beats Scissors";
         }
-    } else if (ComputerChoice == "paper"){
-        if (playerChoice == "rock"){
+    } else if (compSelection == "paper"){
+        if (playerSelection == "rock"){
             return "You lose! Paper beats Rock";
-        } else if (playerChoice == "scissors"){
+        } else if (playerSelection == "scissors"){
             return "You win! Scissors beats Paper";
         }
-    } else if (ComputerChoice == "scissors"){
-        if (playerChoice == "rock"){
-            return "You win! Rock beats Scissors";
-        } else if (playerChoice == "paper"){
+    } else if (compSelection == "scissors"){
+        if (playerSelection == "rock"){
+			return "You win! Rock beats Scissors";
+        } else if (playerSelection == "paper"){
             return "You lose! Scissors beats Paper";
         }
     }
-    return `The round was a Draw, both selected ${playerChoice}`;
+    return `The round was a Draw, both selected ${playerSelection}`;
 }
-console.log("Computer:",compSelection);
-console.log("You:",playerSelection);
-console.log(playRound(compSelection, playerSelection));
+
+function game() {
+	var yourScore = 0;
+	var compScore = 0;
+
+    for (let i = 0; i < NUMBER_OF_ROUNDS; i++) {
+		console.log("Ronud", i + 1)
+		var round = playRound();
+    	console.log(round);
+		if (round.includes("win!")) {
+			yourScore ++;
+		} else if (round.includes("lose!")) {
+			compScore ++;
+		}
+		console.log(`You ( ${yourScore} ) : Computer ( ${compScore} ) \n `)
+    }
+	if (yourScore > compScore) {
+		return `Congratulations YOU WON!!! \n Final Score You ( ${yourScore} ) : Computer ( ${compScore} )`
+	} else if (yourScore < compScore) {
+		return `You Lost :( \n Final Score You ( ${yourScore} ) : Computer ( ${compScore} )`
+	} else {
+		return `Draw \n Final Score You ( ${yourScore} ) : Computer ( ${compScore} )`
+	}
+}
+console.log(game())
